a4e8b2497d87ea0ab2e01d65d39367dabcf086ad
Burgerballs <107233412+Burgerballs@users.noreply.github.com>
2024-05-15 17:17:09 +0100

    multiplicative added!! + sum tween fixes

--- source/funkin/play/PlayState.hx
+++ source/funkin/play/PlayState.hx
@@ -3260,6 +3260,8 @@ class PlayState extends MusicBeatSubState
     cancelCameraZoomTween();
   }
 
+  var prevScrollTargets:Array<Dynamic> = []; // used to snap scroll speed when things go unruely
+
   /**
    * The magical function that shall tween the scroll speed.
    */
@@ -3267,6 +3269,18 @@ class PlayState extends MusicBeatSubState
   {
     // Cancel the current tween if it's active.
     cancelScrollSpeedTweens();
+
+    // Snap to previous event value to prevent the tween breaking when another event cancels the previous tween.
+    for (i in prevScrollTargets)
+    {
+      var value:Float = i[1];
+      var strum:Strumline = Reflect.getProperty(this, i[0]);
+      strum.scrollSpeed = value;
+    }
+
+    // for next event, clean array.
+    prevScrollTargets = [];
+
     for (i in strumlines)
     {
       var value:Float = speed;
@@ -3283,6 +3297,8 @@ class PlayState extends MusicBeatSubState
             'scrollSpeed': value
           }, duration, {ease: ease}));
       }
+      // make sure charts dont break if the charter is dumb and stupid
+      prevScrollTargets.push([value, i]);
     }
   }
 
